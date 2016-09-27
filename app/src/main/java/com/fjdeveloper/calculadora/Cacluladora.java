37fbb7209d20528975197914f2e77265410ad0db
Fernando Fdez <fjfearroyo@gmail.com>
2016-09-27 17:49:38 +0200

    Initial commit

---
+++ app/src/main/java/com/fjdeveloper/calculadora/Cacluladora.java
@@ -0,0 +1,13 @@
+package com.fjdeveloper.calculadora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Cacluladora extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cacluladora);
+    }
+}
